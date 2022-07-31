a2f1d157a1739c0d4a1d16dfc0ce89a5004a788f
Aleksandr <alexander.savichev@gmail.com>
2022-07-31 22:44:24 +0300

    Добавление чеков в строку, а не столбец + отображение с новой строки, а не с первой

--- src/main/java/com/example/evotor/Controllers/Controller.java
+++ src/main/java/com/example/evotor/Controllers/Controller.java
@@ -2,15 +2,15 @@ package com.example.evotor.Controllers;
 import com.example.evotor.Models.EvotorReceiptRequest;
 import com.example.evotor.Service.SheetsServiceUtil;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+
 
 
 @RestController
@@ -22,15 +22,21 @@ public class Controller {
         sheetsService = SheetsServiceUtil.getSheetsService();
         ValueRange body = new ValueRange()
                 .setValues(Arrays.asList(
-                        Arrays.asList(evotorReceiptRequest.id),
-                        Arrays.asList(evotorReceiptRequest.timestamp),
-                        Arrays.asList(evotorReceiptRequest.userId),
-                        Arrays.asList(evotorReceiptRequest.type),
-                        Arrays.asList(evotorReceiptRequest.version)
+                        Arrays.asList(evotorReceiptRequest.id,
+                                evotorReceiptRequest.timestamp,
+                                evotorReceiptRequest.userId,
+                                evotorReceiptRequest.type,
+                                evotorReceiptRequest.version )
                 ));
-        UpdateValuesResponse result = sheetsService.spreadsheets().values()
-                .update(SPREADSHEET_ID, "A1", body)
-                .setValueInputOption("RAW")
-                .execute();
+       AppendValuesResponse result = sheetsService.spreadsheets().values()
+               .append(SPREADSHEET_ID, "A1", body)
+               .setValueInputOption("USER_ENTERED")
+               .setInsertDataOption("INSERT_ROWS")
+               .setIncludeValuesInResponse(true)
+               .execute();
+
+        ValueRange total = result.getUpdates().getUpdatedData();
+        assert(total.getValues().get(0).get(1)).equals("65");
+
     }
 }
